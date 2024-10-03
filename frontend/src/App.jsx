eeb8ffcb8f2f7ad9297a664f4a9c71e90a8b3782
Mohd. Shayan <sehbaqureshi@gmail.com>
2024-10-03 18:05:38 +0530

    Add My Inventory project with frontend and backend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import { Box, useColorModeValue } from "@chakra-ui/react";
+
+import { Route, Routes } from "react-router-dom";
+
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+
+import Navbar from "./components/Navbar";
+
+function App() {
+  const bgColor = useColorModeValue("gray.300", "gray.800");
+  return (
+    <>
+      <Box minH={"100vh"}  bg={bgColor}>
+        <Navbar/>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreatePage />} />
+        </Routes>
+      </Box>
+    </>
+  );
+}
+
+export default App;
